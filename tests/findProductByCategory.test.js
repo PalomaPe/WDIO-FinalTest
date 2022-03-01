dd8ff30173342c5a34561beb788f704d2009ebb5
Paloma Peirano <paloma20.11@hotmail.com>
2022-03-01 13:06:14 -0300

    fourth test

--- tests/findProductByCategory.test.js
+++ tests/findProductByCategory.test.js
@@ -1,24 +1,24 @@
 import HomePage from '../pages/homePage.js';
+import CataloguePage from '../pages/cataloguePage.js';
 import CATEGORIES from '../data/categories.js';
 import saveArticlesNamesByCategory from '../helpers/handleData.js';
 
 describe('Find products under their category: DRESSES or T-SHIRTS', () => {
     CATEGORIES.forEach(({ category, typeOfProduct }) => {
-        
-        console.log(category + ' category');
-        console.log(typeOfProduct + ' typeofproduct')
         it(`It should allow the user to find all products under their respective category`, async () => {
-            let articlesNames = [];
             await HomePage.abrir('/');
             await HomePage.goSubCategoryInWoman(category);
-            articlesNames = await $$(`//a[@class='product-name']`);
-            articlesNames = await articlesNames.filter(name => name != {});
-            Promise.all(articlesNames.map(async name => await name.getText()))
-                .then(names => names.filter(name => typeOfProduct.some(type => name.toLowerCase().includes(type))))
-                //.then(filteredNames => expect(filteredNames.length).to.equal(articlesNames.length))
-                .then(names => saveArticlesNamesByCategory(category, names))
-                .catch((err) => console.log(err)); // 0;;
-            browser.pause(50000)
+            const articlesNames = await CataloguePage.articlesNames;
+            
+            await Promise.all(articlesNames.map(async name => await name.getText()))
+                .then(names => {
+                    return names.filter(name => typeOfProduct.some(type => name.toLowerCase().includes(type)))
+                })
+                .then(async filtered => {
+                    await saveArticlesNamesByCategory(category, filtered),
+                    await expect(await filtered.length).to.equal(articlesNames.length) // 0
+                })
+                .catch((err) => console.log(err));
         });
 
     })
