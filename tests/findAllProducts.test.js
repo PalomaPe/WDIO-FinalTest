ffb7933692a9412d4238638e7e7805b0af9ccc89
Paloma Peirano <paloma20.11@hotmail.com>
2022-03-01 13:06:21 -0300

    fourth test

--- tests/findAllProducts.test.js
+++ tests/findAllProducts.test.js
@@ -20,28 +20,3 @@ describe(`Find all expected products under the category ${category}`, () => {
     })
 
 });
-
-describe('Find products under their category: DRESSES or T-SHIRTS', () => {
-    CATEGORIES.forEach(({ category, typeOfProduct }) => {
-
-        console.log(category + ' category');
-        console.log(typeOfProduct + ' typeofproduct')
-        it(`It should allow the user to find all products under their respective category`, async () => {
-            await HomePage.abrir('/');
-            await HomePage.goSubCategoryInWoman(category);
-            let articlesNames = await cataloguePage.articles;
-            
-            await Promise.all(articlesNames.map(async name => await name.getText()))
-                .then(names => {
-                    return names.filter(name => typeOfProduct.some(type => name.toLowerCase().includes(type)))
-                })
-                .then(async filtered => {
-                    await saveArticlesNamesByCategory(category, filtered),
-                    await expect(await filtered.length).to.equal(articlesNames.length) // 0
-                })
-                .catch((err) => console.log(err));
-
-        });
-
-    })
-});
